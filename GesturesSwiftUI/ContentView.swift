8b92fcc4c4048db68df49752e8fa83bcce30d651
Gustavo Antunes <gustavoamalheiros@gmail.com>
2022-02-13 11:15:47 -0300

    Initial Commit

---
+++ GesturesSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GesturesSwiftUI
+//
+//  Created by Gustavo Malheiros on 13/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GesturesSwiftUI/GesturesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GesturesSwiftUIApp.swift
+//  GesturesSwiftUI
+//
+//  Created by Gustavo Malheiros on 13/02/22.
+//
+
+import SwiftUI
+
+@main
+struct GesturesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
